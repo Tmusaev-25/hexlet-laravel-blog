744886fd14735ae5c4d98990ee78f391fe498472
Tima <tmusayev@cic.kz>
2022-01-25 12:12:41 +0600

    corrected

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//
+//Route::get('/', function () {
+//    return 'hello, world!';
+//});
+
+Route::get('about', function () {
+    return view('about');
+});
